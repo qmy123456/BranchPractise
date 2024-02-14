7923105d46052e96a542b66cdf89019d702690ca
QMY\qmywe <my.qian@wesoft.co.jp>
2024-02-14 16:09:45 +0900

    xiaomingCoding

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -10,4 +10,9 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public void xiaomingCoding()
+    {
+        System.out.println( "xiaomingCoding xiaomingCoding xiaomingCoding xiaomingCoding" );
+    }
 }
